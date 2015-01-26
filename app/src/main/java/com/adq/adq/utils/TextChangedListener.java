383d5c58988ed83bbc505c19ddd64d88700fb0cf
joseluisrivas <jlrivas123@gmail.com>
2015-01-25 22:01:23 -0500

    Se Agrego captura propiedad

---
+++ app/src/main/java/com/adq/adq/utils/TextChangedListener.java
@@ -0,0 +1,25 @@
+package com.adq.adq.utils;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Metodo para escuchar todas las acciones
+ * Created by root on 25/01/15.
+ */
+public class TextChangedListener implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
